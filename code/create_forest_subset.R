cfca4ab5a6ec7991b73f3760c6cd1caf74493624
SimonCMills <smills2@sheffield.ac.uk>
2020-10-23 12:27:37 +0100

    minor edits

--- code/create_forest_subset.R
+++ code/create_forest_subset.R
@@ -1,5 +1,9 @@
-## Create forest species subset 
+## Create forest species subset from the PSF database. Forest specialists are 
+## defined as those species that are present in forest, but never present in 
+## forest edge, secondary forest, and pasture. 
+
 library(dplyr); library(ggplot2)
+
 parker_lookup <- readRDS("data/Parker_Stotz_Fitzpatrick_1996/parker_lookup_subset.rds")
 adata <- read.csv("data/Parker_Stotz_Fitzpatrick_1996/databases/adata.csv") %>%
     as_tibble %>%
@@ -8,16 +12,16 @@ adata <- read.csv("data/Parker_Stotz_Fitzpatrick_1996/databases/adata.csv") %>%
 species_lookup <- read.csv("data/initial_species_list.csv") %>%
     left_join(., parker_lookup) %>%
     as_tibble %>%
-    mutate(Species_HBW = gsub(" ", "_", HBW)) %>%
-    select(Species_HBW, eBird, HBW, Donegan, Pulido, eltontraits, parker)
+    mutate(species_hbw = gsub(" ", "_", HBW)) %>%
+    select(species_hbw, eBird, HBW, Donegan, Pulido, eltontraits, parker)
 
 df_bird <- readRDS("../Colombia/data/bird data_Jan&Jun2019/analysisDataset_EasternCordillera.rds") 
 
 # Drops 1 species: Columba livia
 df_species <- df_bird %>% 
-    group_by(Species_HBW) %>%
-    summarise(n_tot = n(), n_pt = length(unique(Point))) %>%
-    select(Species_HBW, n_tot, n_pt) %>%
+    group_by(species_hbw) %>%
+    summarise(n_tot = n(), n_pt = length(unique(point))) %>%
+    select(species_hbw, n_tot, n_pt) %>%
     arrange(desc(n_pt)) %>%
     left_join(., species_lookup) %>%
     left_join(., adata) 
@@ -27,26 +31,25 @@ dropped_species <- df_species %>%
 dropped_species
 
 df_summ <- df_bird %>% 
-    group_by(Species_HBW) %>%
+    group_by(species_hbw) %>%
     mutate(n_tot = n()) %>%
-    group_by(Species_HBW, n_tot, Point) %>%
+    group_by(species_hbw, n_tot, point) %>%
     summarise(Q = 1) %>%
-    group_by(Species_HBW, n_tot) %>%
+    group_by(species_hbw, n_tot) %>%
     summarise(n_pt = sum(Q)) %>%
     arrange(desc(n_pt))
 
-library(ggplot2)
-ggplot(df_summ %>% filter(n_pt<10), aes(n_pt, n_tot)) + geom_jitter(height=.2, width=.2) + 
-    scale_y_continuous(breaks=seq(0, 20, 2)) +
-    scale_x_continuous(breaks = seq(0, 20, 2)) + 
-    coord_equal()
+ggplot(df_summ %>% filter(n_pt<=20), aes(n_pt, n_tot/4/n_pt)) + geom_jitter(height=0, width=.2) + 
+    scale_y_continuous("N observations/N points", breaks=seq(0, 1, .25)) +
+    scale_x_continuous("N points observed", breaks = seq(0, 20, 2)) + 
+    coord_fixed(5)
 
 
-df_habcat <- df_species %>% select(Species_HBW, n_pt, n_tot, HAB1:HAB7) %>%
-    reshape2::melt(., id.vars=c("Species_HBW", "n_pt", "n_tot")) %>%
-    select(Species_HBW, n_pt, n_tot, order=variable, habitat=value)
+df_habcat <- df_species %>% select(species_hbw, n_pt, n_tot, HAB1:HAB7) %>%
+    reshape2::melt(., id.vars=c("species_hbw", "n_pt", "n_tot")) %>%
+    select(species_hbw, n_pt, n_tot, order=variable, habitat=value)
 
-df_habcat_summ <- df_habcat %>% group_by(Species_HBW, n_pt, n_tot) %>%
+df_habcat_summ <- df_habcat %>% group_by(species_hbw, n_pt, n_tot) %>%
     summarise(any_forest = any(habitat %in% paste0("F", 1:14)), 
               sec_and_forest_edge = any(habitat %in% paste0("F", 15)|habitat %in% paste0("F", 1:15, "E")), 
               pasture = any(habitat %in% paste0("N", 1:15)|habitat %in% paste0("N", 1:15, "E")),
