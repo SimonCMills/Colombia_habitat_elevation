1ae57c0239738ec271ac0b106609b63ef8cbbe40
SimonCMills <smills2@sheffield.ac.uk>
2021-03-22 14:22:14 +0000

    updated map plotting code to ~final version

--- code/get_elevation_forestcover_raster.R
+++ code/get_elevation_forestcover_raster.R
@@ -4,11 +4,6 @@
 ## Packages ----
 library(rgee); library(sf); library(dplyr)
 
-## remove prior versions
-file.remove(list.files("D:/Drive/rgee_exports/", 
-                       pattern="forest cover E Cord_1km|elevation E Cord_500m", 
-                       full.names = TRUE))
-
 ## Set up rgee session ----
 ee_Initialize()
 
@@ -19,7 +14,7 @@ tc <- ee$Image("UMD/hansen/global_forest_change_2019_v1_7")$select("treecover200
 ## Export ele and fc from earth engine ----
 df_ptInfo <- readRDS("data/point_ele_Eastern Cordillera.rds") %>%
     filter(forest==1) %>%
-    st_transform(crs = flat)
+    st_transform(crs = "epsg:3117")
 
 bbox <- st_bbox(df_ptInfo) %>%
     # st_geometry() %>%
@@ -33,34 +28,51 @@ bbox_rgee <- sf_as_ee(bbox)
 
 # clip rasters
 # ALOS_flattened <- ALOS$clip(bbox_rgee)
-ALOS_clipped <- ALOS$clip(bbox_rgee)
+ALOS_clipped <- ALOS$reproject(crs=tc$projection())$clip(bbox_rgee)
 tc_clipped <- tc$clip(bbox_rgee)
 
 # calculate forest cover at a 1000 m resolution (~corresponds to the 500 m radius)
-forest <- tc$gte(50)
+forest <- tc_clipped$gte(50)
 
-forestCover <- forest$reduceResolution(
+forestCover <- tc_clipped$reduceResolution(
     reducer = ee$Reducer$mean(),
     maxPixels = 2000
-)$reproject(crs = forest$projection(), scale=1000)
+)$reproject(crs = "epsg:3117", scale=500)
 
-# note dropping scale to 5k for purposes of plot
-export_fc <- ee_image_to_drive(image = forestCover, 
-                               description = "forest cover E Cord_1km", 
-                               folder = "rgee_exports",
-                               region = bbox_rgee, 
-                               crs = "epsg:3117", # projected CRS for Colombia
-                               maxPixels = 1e9)
-export_fc$start()
-ee_monitoring(export_fc)
+ele_500m <- ALOS_clipped$reduceResolution(
+    reducer = ee$Reducer$mean(),
+    maxPixels = 2000
+)$reproject(crs = "epsg:3117", scale=500)
+    
 
-## Elevation 
-export_ele <- ee_image_to_drive(image = ALOS_clipped, 
-                                description = "elevation E Cord_500m", 
-                                folder = "rgee_exports",
-                                scale = 500, # drop resolution
-                                region = bbox_rgee, 
-                                crs = "epsg:3117", # projected CRS for Colombia
-                                maxPixels = 1e9)
-export_ele$start()
-ee_monitoring(export_ele)
+fc_stars <- ee_as_stars(forestCover)
+ele_stars <- ee_as_stars(ele_500m)
+
+write_stars(ele_stars, "data/ele_fc_rasters/elevation_500m.tif")
+write_stars(fc_stars, "data/ele_fc_rasters/fc_500m.tif")
+
+# 
+# plot(ele_stars)
+# ele_stars
+# fc_stars
+# # export
+# export_fc <- ee_image_to_drive(image = forestCover, 
+#                                description = "forest cover E Cord_500m", 
+#                                folder = "rgee_exports",
+#                                crs = "epsg:3117", # projected CRS for Colombia
+#                                region = bbox_rgee, # redundant.. 
+#                                scale = 500, 
+#                                maxPixels = 1e9)
+# export_fc$start()
+# ee_monitoring(export_fc)
+# 
+# ## Elevation 
+# export_ele <- ee_image_to_drive(image = ALOS_clipped, 
+#                                 description = "elevation E Cord_500m", 
+#                                 folder = "rgee_exports",
+#                                 crs = "epsg:3117", # projected CRS for Colombia
+#                                 scale = 500, # drop resolution
+#                                 region = bbox_rgee, # redundant..
+#                                 maxPixels = 1e9)
+# export_ele$start()
+# ee_monitoring(export_ele)
--- code/plot_map.R
+++ code/plot_map.R
@@ -1,95 +1,223 @@
-##
-library(mapview); library(leaflet); library(dplyr); library(sf); library(htmltools)
-library(ggspatial)
+## plotting map of points. 
+# note: needs post-processing to move inset figure to overlap plot (not 
+# straightforward to do this without leaving plot margins of first plot running
+# through inset figure; solution was to plot adjacent to figure and move outside
+# of R)
+
+# housekeeping
+library(dplyr); library(sf); library(stars); library(ggplot2); 
+library(rnaturalearthhires); library(rnaturalearth)
 WGS84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 flat <- "epsg:3117"
-# create colours for points
-col_lookup <- data_frame(type = c("F", "P", "A"),
-                         cols = c("#32827B", "#EEDFB8", "#9F7032"))
+recalc_rasters <- FALSE
 
-df_bird <- readRDS("../Colombia/data/bird data_Jan&Jun2019/analysisDataset_EasternCordillera.rds") %>%
-    st_transform(crs = flat)
-unique_pt <- df_bird$point %>% unique
+# only run if you want to regenerate rasters (takes 5 minutes or so)
+if(recalc_rasters) {
+    source("code/get_elevation_forestcover_raster.R")
+}
 
-# weird- corrupted or something
+# read rasters ----
+ele <- read_stars("data/ele_fc_rasters/elevation_500m.tif")
+fc <- read_stars("data/ele_fc_rasters/fc_500m.tif")
+
+# read points ----
 df_ptInfo <- readRDS("data/point_ele_Eastern Cordillera.rds") %>%
-    filter(forest==1) %>%
-    st_transform(crs = flat)
-#df_ptInfo[287,]
+    filter(forest==1, !grepl("^CC", point)) %>%
+    st_transform(crs = "epsg:3117")
 
-library(raster)
-ele <- raster("data/elevation_Colombia_3.tif")
-ele2 <- flip(ele, direction="y")
-ele_df <- rasterToPoints(ele) %>% 
-    as_tibble() #%>%
-    #st_as_sf(., coords=c("x", "y"), crs=WGS84) #%>%
-    # st_transform(crs = flat)
-# ele_df <- bind_cols(ele_df, as_tibble(st_coordinates(ele_df)))
+pts <- st_coordinates(df_ptInfo) %>% as_tibble
+
+# convert rasters and clip to bbox ----
+bbox <- st_bbox(df_ptInfo) %>%
+    st_as_sfc %>%
+    st_buffer(45000) %>% # note: buffer by 50 km, but in plot buffer by 30 km
+    st_bbox() %>%
+    st_as_sfc()
+
+ele_df <- ele %>% 
+  st_crop(bbox) %>%
+  st_as_sf(., as_points = TRUE, merge = FALSE)
+
+fc_df <- fc %>% 
+  st_crop(bbox) %>%
+  st_as_sf(., as_points = TRUE, merge = FALSE) 
+
+both <- st_join(fc_df, ele_df) %>% 
+    rename(fc = 1, ele = 2) %>%
+    bind_cols(., as_tibble(st_coordinates(.))) %>%
+    mutate(ele_f = cut(ele, seq(0, 5000, 1000)))
+
+# colour scales ----
+greens <- RColorBrewer::brewer.pal(9, "Greens")
+n_levels <- length(levels(both$ele_f))
+fc_cols <- colorRampPalette(greens[3:9])(n_levels)
+ele_cols <- colorRampPalette(c("grey90", "grey10"))(n_levels)
+
+# create separate rasters for plot ----
+fc_df_for_plot <- both %>%
+    filter(fc > 30) %>%
+  as_tibble
+
+ele_clip <- both %>%
+    filter(ele >= 4000)
+
+# other features ----
+V_border <- ne_countries(country="Colombia", scale=10) %>% 
+  st_as_sf() %>%
+  st_transform(., flat) %>%
+  st_crop(., bbox) %>%
+  as_Spatial() %>%
+  fortify(.)
+
+Bogota_df <- data_frame(y=4.7110, x=-74.0721) %>%
+  st_as_sf(., coords=c("x", "y"), crs=WGS84) %>%
+  st_transform(., crs=flat) %>%
+  mutate(label = "Bogotá")%>%
+  st_coordinates(.) %>%
+  as_tibble
+
+# get coordinates for margins ----
+x_coords <- expand.grid(long_WGS84 = seq(-74.5, -72.5, 1), lat_WGS84 = dimensions_WGS84["ymin"]) %>%
+  as.matrix
+y_coords <- expand.grid(long_WGS84 = dimensions_WGS84["xmin"], lat_WGS84 = seq(4, 8, 2)) %>%
+  as.matrix
+
+x_sp <- x_coords %>% 
+  st_multipoint() %>%
+  st_geometry
+st_crs(x_sp) <- WGS84
+x_coords_flattened <- st_transform(x_sp, flat) %>% 
+  st_coordinates() %>% 
+  as_tibble
+
+## now y
+y_sp <- y_coords %>% 
+  st_multipoint() %>%
+  st_geometry
+st_crs(y_sp) <- WGS84
+
+y_coords_flattened <- st_transform(y_sp, flat) %>% 
+  st_coordinates() %>% 
+  as_tibble
+
+y_trans <- bind_cols(as_tibble(y_coords), y_coords_flattened) %>%
+  mutate(lat_WGS84 = paste0(lat_WGS84, "\u00B0"))
+x_trans <- bind_cols(as_tibble(x_coords), x_coords_flattened) %>%
+  mutate(long_WGS84 = paste0(long_WGS84, "\u00B0"))
+
+# plot 1 ----
+map_fc <- ggplot(fc_df_for_plot) + 
+  geom_tile(data=ele_clip, aes(X, Y), fill="grey80") +
+  geom_tile(aes(X, Y, fill=ele_f)) +
+  geom_polygon(data = V_border, aes(long, lat), fill=NA, col="black") + #geom_tile(aes(X, Y, fill = ele_f)) +
+  scale_fill_manual(values=fc_cols) +
+  geom_point(data=pts, aes(X, Y), pch=21, fill="grey90", size=2) +
+  scale_x_continuous(expand=c(0,0), 
+                     breaks=pull(x_trans, X), 
+                     labels = pull(x_trans, long_WGS84)) + 
+  scale_y_continuous(expand=c(0,0),
+                     breaks=pull(y_trans, Y), 
+                     labels = pull(y_trans, lat_WGS84)) +
+  labs(x="Lat", y="Long", fill="Elevation") +
+  geom_point(data=Bogota_df, aes(X, Y), size=3, shape=17) + 
+  # geom_label(data=Bogota_df, aes(X,Y+20000), label="Bogotá", label.size=0, fill=NA) +
+  theme_bw() +
+  theme(axis.title = element_blank(), 
+        axis.text=element_text(colour="black"), 
+        panel.grid = element_blank()) +
+  guides(fill="none") +
+  coord_equal() 
+
+# plot 2 ----
+hist_fc <- ggplot(df_ptInfo, aes(ele_jaxa, fill=cut(ele_jaxa, seq(0, 5000, 1000)))) + 
+  geom_histogram(binwidth=200, col="black", boundary=0) +
+  scale_fill_manual(values = fc_cols) +
+  scale_y_continuous(expand=c(0,0), breaks=c(0, 20, 40)) +
+  scale_x_continuous(expand = c(0,0)) +
+  labs(x="", y="") +
+  guides(fill="none") +
+  theme(axis.text = element_text(colour="black"), 
+        panel.background = element_blank(), 
+        plot.background = element_blank(), 
+        panel.grid = element_blank(), 
+        axis.text.x = element_text(margin=margin(2,0,0,0))) 
+
+# Plot 3 ----
+# create bounding box for inset figure (all of CO plus outlying areas)
+# just removes far-away areas to prevent self intersections when projected for 
+# Colombia
+left <- -90
+right <- -60
+top <- 13
+bottom <- -8
+bbox_inset <- matrix(c(left, top, 
+                       left, bottom, 
+                       right, bottom, 
+                       right, top, 
+                       left, top), 
+                     byrow=T, ncol=2) %>%
+    list() %>%
+    st_polygon() %>%
+    st_sfc(., crs="WGS84")
+
+# get Colombia bounding box (this will be the extent of the inset plot)
+CO_buffer <- ne_countries(country="Colombia") %>%
+    st_as_sf %>%
+    st_transform(., crs=flat) %>%
+    st_buffer(50000) %>%
+    st_bbox()
+
+test <- rnaturalearth::ne_countries(scale=10) %>%
+  st_as_sf %>%
+  st_crop(., bbox_inset) %>%
+  st_transform(., crs=flat) %>% 
+  st_crop(., CO_buffer)
 
 Bogota <- data_frame(y=4.7110, x=-74.0721) %>%
     st_as_sf(., coords=c("x", "y"), crs=WGS84) %>%
     st_transform(., crs=flat) %>%
-    st_coordinates(.) %>%
-    as_tibble
-#scalebar
-scale <- tibble(x=c(945000 - 105000, 945000 - 55000), y=c(826000 + 90000,826000 + 90000))
+    mutate(label = "Bogotá")
+    
+# get mountain ranges
+mountains <- readRDS("../range_shifts/data/mountain_polygons.rds") %>% 
+      st_union() %>%
+      st_crop(., bbox_inset) %>%
+      st_transform(., crs=flat) %>%
+      st_buffer(., 1) %>%
+      st_crop(CO_buffer)
+  
+mountains_ftfy <- mountains %>%
+  as_Spatial %>%
+  fortify()
 
-rgdal::ogrListLayers("../Colombia/col_admbnda_adm0-1-2.gdb/col_admbnda_adm0-1-2.gdb")
-polys <- st_read("../Colombia/col_admbnda_adm0-1-2.gdb/col_admbnda_adm0-1-2.gdb",
-                 layer="col_admbnda_adm1_unodc_ocha") #%>%
-    # filter(admin1Name_es %in% c("Boyacá", "Cundinamarca"))
+# Plot inset figure (3) ---
+mapplot <- ggplot(Bogota) + 
+    geom_sf(data=mountains, fill="grey90", col=NA) +
+    geom_sf(data=test, fill=NA, col="black", alpha=1) +
+    geom_sf(data=bbox, fill=NA, col="black", alpha=1) +
+    theme_void() +
+    geom_sf(size=2, shape=17, col="black", alpha=1)
 
-clipped <- st_crop(polys %>% st_transform(crs(ele)), ele) %>%
-    st_transform(flat) %>% 
-    st_union()
+ggsave("figures/map_inset.png", mapplot, height=200*.2, width=150*.2, units="mm")
 
-pts <- st_coordinates(df_ptInfo) %>% as_tibble
-p1 <- ggplot(ele_df) + 
-    geom_tile(aes(x, y, fill=elevation_Colombia_3)) + 
-    scale_fill_gradientn(colours = c("#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF", "#FEE090", "#FDAE61", "#F46D43", "#D73027")) +
-    geom_sf(data=clipped, fill=NA, col="grey20") +
-    geom_point(data=pts, aes(X, Y), pch=21, fill="#A5D6A7", size=2) +
-    scale_x_continuous(expand=c(0,0), breaks=-72.5:-74.5) + #limits = c(5.9e+05, NA)) + 
-    scale_y_continuous(expand=c(0,0)) +
-    labs(x="Lat", y="Long", fill="Elevation") +
-    #geom_line(data=scale, aes(x, y)) +
-    #geom_label(data=data.frame(x=865000, y=916000, label="50 km"), aes(x=x, y=y, label=label), vjust=0, label.size=0, fill=NA) +
-    geom_point(data=Bogota, aes(X, Y), size=2, shape=17) + 
-    geom_label(data=Bogota, aes(X,Y+20000), label="Bogotá", label.size=0, fill=NA) +
-    theme(axis.title = element_blank(), axis.text=element_text(colour="black")) +
-    coord_sf(xlim = c(540000 + 60000, 945000 - 50000), 
-             ylim = c(826000 + 80000, 1426000 - 1000)) +
-    guides(fill=F)
-p1
-# ggsave("figures/map_firstdraft.png")
+# Join all 3 plots ----
+# because the plot is coord_equal() scaled, you have to maintain the dimensions
+# when inserting the grob. 
+dims <- bbox %>% st_bbox()
 
+plot_all3 <- map_fc +
+  theme(plot.margin = unit(c(2, 0, 1, 0), "cm")) +
+  annotation_custom(
+    ggplotGrob(hist_fc),
+    xmin = x_inset[1] + 8000, xmax = x_inset[2] + 15000,
+    ymax = y_trans$Y[1] + 8e4, ymin= y_trans$Y[1] -4e4
+  ) +
+  annotation_custom(
+    ggplotGrob(mapplot), 
+    xmin = 7e+05 - 3e5 , xmax = 7e+05 - 1e5, 
+    ymax = 15e5 + 2e5/(dims["ymax"] - dims["ymin"]), ymin= 13e5 #bbox2["ymin"] 
+  ) 
+plot_all3
 
-ncols <- length(levels(cut(df_ptInfo$ele_jaxa, seq(0, 4000, 100), right=F)))
-cols <- colorRampPalette(c("#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF", 
-                           "#FEE090", "#FDAE61", "#F46D43", "#D73027"))(ncols)
-
-library(ggthemes)
-p2 <- ggplot(df_ptInfo, aes(ele_jaxa, fill=cut(ele_jaxa, seq(0, 4000, 100), right=F))) + 
-    geom_histogram(binwidth=100, col="black", boundary=200) +
-    scale_fill_manual(values = cols) +
-    scale_y_continuous(breaks=seq(0, 100, 10), expand = expansion(mult = 0.01)) +
-    scale_x_continuous(expand=expansion(mult = 0.01)) +
-    labs(x="", y="") +
-    guides(fill=F) +
-    theme(axis.text = element_text(colour="black"), 
-          panel.background = element_blank(), 
-          plot.background = element_blank(), 
-          panel.grid = element_blank()) 
-
-dims <- c(x= c(540000 + 60000, 945000 - 50000), y = c(826000 + 80000, 1426000 - 1000))
-x_inset <- c(dims["x1"] + diff(dims[c("x1", "x2")])/3, dims["x2"] - 8000)
-y_inset <- c(dims["y1"] - 20000, dims["y1"] + 100000 - 20000)
-
-p_a_both <- p1 + 
-    annotation_custom(
-        ggplotGrob(p2), 
-        xmin = x_inset[1] + 5000, xmax = x_inset[2], ymax = y_inset[1], ymin= y_inset[2]
-    )
-
-ggsave("figures/map_and_elevations.png", p_a_both, height=200*.8, width=150*.8, units="mm")
-plot
+ggsave("figures/map_fullplot_v3.png", plot_all3, height=200*.8, width=250*.8, 
+       units="mm", dpi=400)
