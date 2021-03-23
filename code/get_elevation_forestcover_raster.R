c7e99a17750027baa2c0eefa6a91c32fde7441d2
SimonCMills <smills2@sheffield.ac.uk>
2021-03-23 14:54:07 +0000

    minor edits

--- code/get_elevation_forestcover_raster.R
+++ code/get_elevation_forestcover_raster.R
@@ -19,7 +19,7 @@ df_ptInfo <- readRDS("data/point_ele_Eastern Cordillera.rds") %>%
 bbox <- st_bbox(df_ptInfo) %>%
     # st_geometry() %>%
     st_as_sfc %>%
-    st_buffer(50000) %>% # note: buffer by 50 km, but in plot buffer by 30 km
+    st_buffer(50000) %>% 
     st_bbox() %>%
     st_as_sfc()
 
@@ -27,11 +27,10 @@ bbox <- st_bbox(df_ptInfo) %>%
 bbox_rgee <- sf_as_ee(bbox)
 
 # clip rasters
-# ALOS_flattened <- ALOS$clip(bbox_rgee)
 ALOS_clipped <- ALOS$reproject(crs=tc$projection())$clip(bbox_rgee)
 tc_clipped <- tc$clip(bbox_rgee)
 
-# calculate forest cover at a 1000 m resolution (~corresponds to the 500 m radius)
+# calculate forest cover at a 500 m resolution 
 forest <- tc_clipped$gte(50)
 
 forestCover <- tc_clipped$reduceResolution(
@@ -50,29 +49,3 @@ ele_stars <- ee_as_stars(ele_500m)
 
 write_stars(ele_stars, "data/ele_fc_rasters/elevation_500m.tif")
 write_stars(fc_stars, "data/ele_fc_rasters/fc_500m.tif")
-
-# 
-# plot(ele_stars)
-# ele_stars
-# fc_stars
-# # export
-# export_fc <- ee_image_to_drive(image = forestCover, 
-#                                description = "forest cover E Cord_500m", 
-#                                folder = "rgee_exports",
-#                                crs = "epsg:3117", # projected CRS for Colombia
-#                                region = bbox_rgee, # redundant.. 
-#                                scale = 500, 
-#                                maxPixels = 1e9)
-# export_fc$start()
-# ee_monitoring(export_fc)
-# 
-# ## Elevation 
-# export_ele <- ee_image_to_drive(image = ALOS_clipped, 
-#                                 description = "elevation E Cord_500m", 
-#                                 folder = "rgee_exports",
-#                                 crs = "epsg:3117", # projected CRS for Colombia
-#                                 scale = 500, # drop resolution
-#                                 region = bbox_rgee, # redundant..
-#                                 maxPixels = 1e9)
-# export_ele$start()
-# ee_monitoring(export_ele)
